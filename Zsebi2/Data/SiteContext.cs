1a7bd10b5bdeb901a7fc3175cd2e4d6472d18a2b
Viktor Faddi <faddiv@gmail.com>
2018-03-01 19:48:34 +0100

    Simplify SiteContext.

--- Zsebi2/Data/SiteContext.cs
+++ Zsebi2/Data/SiteContext.cs
@@ -5,32 +5,13 @@ namespace Zsebi2.Data
 {
     public class SiteContext : DbContext
 	{
-		//public SiteContext()
-		//{
-
-		//}
-
 		public SiteContext(DbContextOptions<SiteContext> options) : base(options)
         {
 		}
 
-		public DbSet<Article> Articles { get; set; }
-		public DbSet<TeamMember> TeamMembers { get; set; }
-
-
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-		}
-
-		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-	 //  => optionsBuilder
-		//   .UseMySql(@"Server=localhost;database=zsebi;uid=root;pwd=admin;");
+	    public DbSet<Article> Articles => Set<Article>();
+		public DbSet<TeamMember> TeamMembers => Set<TeamMember>();
+        
 	}
-
-	public class News
-	{
-		public int id { get; set; }
-		public string newstitle { get; set; }
-		public string newstext { get; set; }
-	}
+    
 }
