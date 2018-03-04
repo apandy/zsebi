779ea39415df957fe8d5339d26a0ccbf3e3e25e8
Viktor Faddi <faddiv@gmail.com>
2018-03-04 15:35:00 +0100

    Bundling and minifying public pages css and js.

---
+++ Zsebi2/wwwroot/js/zsebi.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    //Index news section.
+    function attachAjaxCalls() {
+        $('#news_container').on('click', '#news-next-container a', function (e) {
+            e.preventDefault();
+            $('#news_container')
+                .load($(this).attr('href'));
+        });
+
+        $('#news_container').on('click', '#news-prev-container a', function (e) {
+            e.preventDefault();
+            $('#news_container')
+                .load($(this).attr('href'));
+        });
+    }
+
+    function attachPopups() {
+        $('a.popup').on('click', function (e) {
+            e.preventDefault();
+            var id = 'link-' + ($(this).index() + 1);
+            $('<div/>', { 'class': 'myDlgClass', 'id': id, 'width': '30%' })
+                .load($(this).attr('href'), function () {
+                    $('a.closeButton').on('click', function (e) {
+                        e.preventDefault();
+                        $(this).closest(".myDlgClass").dialog('close');
+                    });
+                })
+                .appendTo('body')
+                .dialog()
+                .position({
+                    my: "center top",
+                    at: "center top",
+                    of: window
+                });
+        });
+    }
+    
+    $(function() {
+        attachAjaxCalls();
+        attachPopups();
+    });
+
+})();
