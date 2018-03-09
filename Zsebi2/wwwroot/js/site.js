37eca4ae143784eefcf78ae487343d7db6122b39
Viktor Faddi <faddiv@gmail.com>
2018-03-09 20:22:39 +0100

    Resolve #18 Added character counter to edit excerpt.

--- Zsebi2/wwwroot/js/site.js
+++ Zsebi2/wwwroot/js/site.js
@@ -1,1 +1,25 @@
-﻿// Write your JavaScript code.
+﻿(function () {
+
+    function initCharacterCounter() {
+        $("[data-ch-count-for]").each(function () {
+            var $view = $(this);
+            var maxChar = parseInt($view.text());
+            var $target = $($view.data("chCountFor"));
+
+            function setLength() {
+                var content = $target.val();
+                var currentLength = (content || "").length;
+                $view.text(maxChar - currentLength);
+            }
+
+            $target.on("change", setLength);
+            $target.on("keyup", setLength);
+            setLength();
+        });
+    }
+
+    $(function () {
+        initCharacterCounter();
+    });
+
+})();
