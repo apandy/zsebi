9a648cb5e9e87ebb5f1e842bca7a12b7f9de7d6a
Viktor Faddi <faddiv@gmail.com>
2018-03-09 19:52:59 +0100

    Resolve #20 Testing Menu and Satellite fixation in mobile view.

---
+++ Zsebi2/Controllers/TestingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zsebi2.Controllers
+{
+    public class TestingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ThermalCamera()
+        {
+            return View("_ThermalCamera");
+        }
+
+        public IActionResult ThermalVacuumChamber()
+        {
+            return View("_ThermalVacuumChamber");
+        }
+
+        public IActionResult Shaker()
+        {
+            return View("_Shaker");
+        }
+
+        public IActionResult BalloonFlight()
+        {
+            return View("_BalloonFlight");
+        }
+    }
+}
