8744fa473c1688ff7ae30cdb0e84df0dad4935ac
Viktor Faddi <faddiv@gmail.com>
2018-03-09 19:26:27 +0100

    Move Satellite part separate into separate controller.

--- Zsebi2/Controllers/HomeController.cs
+++ Zsebi2/Controllers/HomeController.cs
@@ -33,31 +33,6 @@ namespace Zsebi2.Controllers
         {
             return PartialView("_ContactTeam");
         }
-        public IActionResult SatelliteEngineering()
-        {
-            return PartialView("_SatelliteEngineering");
-        }
-        public IActionResult SatelliteEPS1()
-        {
-            return PartialView("_SatelliteEPS1");
-        }
-        public IActionResult SatelliteEPS2()
-        {
-            return PartialView("_SatelliteEPS2");
-        }
-        public IActionResult SatelliteOBC()
-        {
-            return PartialView("_SatelliteOBC");
-        }
-        public IActionResult SatelliteTID()
-        {
-            return PartialView("_SatelliteTID");
-        }
-        public IActionResult SatelliteCOM()
-        {
-            return PartialView("_SatelliteCOM");
-        }
-
         public IActionResult TechnicalInfo()
         {
             return View();
---
+++ Zsebi2/Controllers/SatelliteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zsebi2.Controllers
+{
+    public class SatelliteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SatelliteEngineering()
+        {
+            return PartialView("_SatelliteEngineering");
+        }
+
+        public IActionResult SatelliteEPS1()
+        {
+            return PartialView("_SatelliteEPS1");
+        }
+
+        public IActionResult SatelliteEPS2()
+        {
+            return PartialView("_SatelliteEPS2");
+        }
+
+        public IActionResult SatelliteOBC()
+        {
+            return PartialView("_SatelliteOBC");
+        }
+
+        public IActionResult SatelliteTID()
+        {
+            return PartialView("_SatelliteTID");
+        }
+
+        public IActionResult SatelliteCOM()
+        {
+            return PartialView("_SatelliteCOM");
+        }
+
+    }
+}
